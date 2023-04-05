84df5f0ea8e7332770a4fbf3db220edd3ea49753
Noel Gitari <gitarinoel@gmail.com>
2023-04-05 11:57:25 +0300

    Fourth commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,74 @@
-const apiUrl = "https://api.thedogapi.com/v1/images/search?limit=10";
-const apiKey = "YOUR_API_KEY"; // Replace with your own API key
+const apiUrl = "https://api.thedogapi.com/v1";
+const apiKey = "live_yteZqRBNj4idAySvdpEyUembQtxUaLc0cY4dcLSkJeYLLVNXmtQ17Pm3Fj1UXzx1"; // Replace with your own API key
 
-fetch(apiUrl, {
-  headers: {
-    "x-api-key": apiKey,
-  },
-})
-  .then((response) => response.json())
-  .then((data) => {
-    const dogsDiv = document.getElementById("dogs");
-    let dogsHtml = "";
-    data.forEach((dog) => {
-      dogsHtml += `
-        <div>
-          <img src="${dog.url}" alt="Dog image">
-          <p>Breed: ${dog.breeds[0].name}</p>
-          <p>Life span: ${dog.breeds[0].life_span}</p>
-          <p>Temperament: ${dog.breeds[0].temperament}</p>
-        </div>
-      `;
-    });
-    dogsDiv.innerHTML = dogsHtml;
-  })
-  .catch((error) => console.error(error));
+// Function to display dog information on the page
+function displayDogInfo(dog) {
+  const dogDiv = document.createElement("div");
+  const dogImage = document.createElement("img");
+  dogImage.src = dog.url;
+  dogImage.alt = "Dog image";
+  const breedParagraph = document.createElement("p");
+  breedParagraph.innerText = `Breed: ${dog.breeds[0].name}`;
+  const lifeSpanParagraph = document.createElement("p");
+  lifeSpanParagraph.innerText = `Life span: ${dog.breeds[0].life_span}`;
+  const temperamentParagraph = document.createElement("p");
+  temperamentParagraph.innerText = `Temperament: ${dog.breeds[0].temperament}`;
+  dogDiv.appendChild(dogImage);
+  dogDiv.appendChild(breedParagraph);
+  dogDiv.appendChild(lifeSpanParagraph);
+  dogDiv.appendChild(temperamentParagraph);
+  document.getElementById("dogs").appendChild(dogDiv);
+}
+
+// Search for dog breed
+document.getElementById("search-button").addEventListener("click", () => {
+  const breed = document.getElementById("search-input").value.trim();
+  if (breed !== "") {
+    fetch(`${apiUrl}/breeds/search?q=${breed}`, {
+      headers: {
+        "x-api-key": apiKey,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        document.getElementById("dogs").innerHTML = "";
+        data.forEach((breedData) => {
+          fetch(`${apiUrl}/images/search?breed_id=${breedData.id}`, {
+            headers: {
+              "x-api-key": apiKey,
+            },
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              data.forEach((dog) => displayDogInfo(dog));
+            })
+            .catch((error) => console.error(error));
+        });
+      })
+      .catch((error) => console.error(error));
+  }
+});
+
+// Upload a dog
+document.getElementById("upload-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const name = document.getElementById("name").value.trim();
+  const breed = document.getElementById("breed").value.trim();
+  const email = document.getElementById("email").value.trim();
+  if (name !== "" && breed !== "" && email !== "") {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("breed", breed);
+    formData.append("email", email);
+    fetch(`${apiUrl}/images/upload`, {
+      method: "POST",
+      headers: {
+        "x-api-key": apiKey,
+      },
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => alert("Dog uploaded successfully!"))
+      .catch((error) => console.error(error));
+  }
+});
