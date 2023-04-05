f9b03f377013e57018a993ce7df1aa3e18ee1517
Noel Gitari <gitarinoel@gmail.com>
2023-04-05 15:21:21 +0300

    Eighth commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,23 +3,25 @@ const apiKey = "live_yteZqRBNj4idAySvdpEyUembQtxUaLc0cY4dcLSkJeYLLVNXmtQ17Pm3Fj1
 
 // Function to display dog information on the page
 function displayDogInfo(dog) {
-  const dogDiv = document.createElement("div");
-  const dogImage = document.createElement("img");
-  dogImage.src = dog.url;
-  dogImage.alt = "Dog image";
-  const breedParagraph = document.createElement("p");
-  breedParagraph.innerText = `Breed: ${dog.breeds[0].name}`;
-  const lifeSpanParagraph = document.createElement("p");
-  lifeSpanParagraph.innerText = `Life span: ${dog.breeds[0].life_span}`;
-  const temperamentParagraph = document.createElement("p");
-  temperamentParagraph.innerText = `Temperament: ${dog.breeds[0].temperament}`;
-  dogDiv.appendChild(dogImage);
-  dogDiv.appendChild(breedParagraph);
-  dogDiv.appendChild(lifeSpanParagraph);
-  dogDiv.appendChild(temperamentParagraph);
-  document.getElementById("dogs").appendChild(dogDiv);
-}
-
+    const dogDiv = document.createElement("div");
+    const dogImage = document.createElement("img");
+    dogImage.src = dog.url;
+    dogImage.alt = "Dog image";
+    const breedParagraph = document.createElement("p");
+    breedParagraph.innerText = `Breed: ${dog.breeds[0].name}`;
+    const lifeSpanParagraph = document.createElement("p");
+    lifeSpanParagraph.innerText = `Life span: ${dog.breeds[0].life_span}`;
+    const temperamentParagraph = document.createElement("p");
+    temperamentParagraph.innerText = `Temperament: ${dog.breeds[0].temperament}`;
+    dogDiv.appendChild(dogImage);
+    dogDiv.appendChild(breedParagraph);
+    dogDiv.appendChild(lifeSpanParagraph);
+    dogDiv.appendChild(temperamentParagraph);
+    document.getElementById("dogs").appendChild(dogDiv);
+  }
+  
+  
+  
 // Search for dog breed
 document.getElementById("search-button").addEventListener("click", () => {
   const breed = document.getElementById("search-input").value.trim();
@@ -31,6 +33,7 @@ document.getElementById("search-button").addEventListener("click", () => {
     })
       .then((response) => response.json())
       .then((data) => {
+        console.log(data)
         document.getElementById("dogs").innerHTML = "";
         data.forEach((breedData) => {
           fetch(`${apiUrl}/images/search?breed_id=${breedData.id}`, {
